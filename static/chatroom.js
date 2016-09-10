d6b78c5176857f45e840fcb7e5b543a76f98a814
yizhi <zhangyiizhi@126.com>
2016-09-10 21:41:35 +0800

    goin on

--- static/chatroom.js
+++ static/chatroom.js
@@ -24,3 +24,25 @@ angular.module('chatroomApp').factory('socket',function($rootScope){
         }
 	}
 })
+
+angular.module('chatroomApp').controller('RoomCtrl',function($scope,socket){
+  $scope.messages = [];
+  socket.emit('getAllMessages')
+  socket.on('allMessages',function(messages){
+    $scope.messages = messages;
+  })
+  socket.on('messageAdded',function(message){
+    $scope.message.push(message);
+  })
+})
+
+angular.module('chatroomApp').controller('MessageCreatorCtrl',function($scope,socket){
+  $scope.newMessage = ''
+  $scope.createMessage = function () {
+    if($scope.newMessage == ''){
+      return
+    }
+    socket.emit('createMessage',$scope.newMessage)
+    $scope.newMessage = ''
+  }
+})
